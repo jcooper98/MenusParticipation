425a0bc95ddf7857113e22ea3fc979b9ed28d6b0
jackcooper <jcooper4@css.edu>
2019-03-10 18:11:36 -0500

    Finished the assignment

--- app/src/main/java/com/jcooper/menusparticipation/MainActivity.java
+++ app/src/main/java/com/jcooper/menusparticipation/MainActivity.java
@@ -100,18 +100,25 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_settings)
+        {
+            SettingsMethod();
+        }
+        else if (id == R.id.nav_add)
+        {
+            AddMethod();
+        }
+        else if (id == R.id.nav_delete)
+        {
+            DeleteMethod();
+        }
+        else if (id == R.id.nav_email)
+        {
+            EmailMethod();
+        }
+        else if (id == R.id.nav_sms)
+        {
+            SMSMethod();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
